7e08b928dea85937cf89f58a838316801178bb5c
rarspace01 <rarspace01@github.com>
2022-08-18 16:15:57 +0200

    cleanup

--- src/main/kotlin/venuebot/Main.kt
+++ src/main/kotlin/venuebot/Main.kt
@@ -3,7 +3,6 @@ package venuebot
 import io.github.bonigarcia.wdm.WebDriverManager
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
-import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.Select
 import org.openqa.selenium.support.ui.WebDriverWait
